6531cdf12e66a2c3d1a8267a5a49ba0f7f79241c
Abdulaziz Al Khater <aks.alkhater@pm.me>
2025-12-08 13:07:13 +0300

    ESP32 finally working.

--- nvim/.config/nvim/lua/plugins/esp32.lua
+++ nvim/.config/nvim/lua/plugins/esp32.lua
@@ -1,7 +1,7 @@
 return {
   "Aietes/esp32.nvim",
   opts = {
-    build_dir = "build.clang",  -- CMake build directory for compile_commands.json
+    build_dir = "build",  -- CMake build directory for compile_commands.json
   },
   dependencies = {
     "folke/snacks.nvim",  -- Required for terminal and picker interfaces
